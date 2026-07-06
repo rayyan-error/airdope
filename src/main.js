e569929030a34230264917a0fd15133c5e301806
Rayyan <mohammad.rayyan@bcah.christuniversity.in>
2026-07-06 17:57:00 +0530

    added cube

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // Importing whats needed to create a 3D scene and render it to the screen. This includes the Three.js library and a CSS file for styling.
 
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from 'three'; 
 
-// This creates a fresh new empty world or workspace for us to add objects, lights, and cameras to. It is the root of the scene graph.
+// Creates an empty 3D world (scene) that will hold every object, light, and camera in our application.
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -12,3 +12,21 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry();
+
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+camera.position.z = 5;
+
+renderer.render(scene, camera);
